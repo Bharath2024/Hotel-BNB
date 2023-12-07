b5c24bf8ded430471c210f02c560afb6652f6dc5
060-NAVEENKUMARM <145565021+Vigneshp30@users.noreply.github.com>
2023-12-07 14:59:42 +0530

    update

--- index.js
+++ index.js
@@ -148,6 +148,10 @@ app.get("/Rooms", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "rooms.html"));
 });
 
+app.get("/chat", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "chat.html"));
+});
+
 app.listen(app.get("port"), function () {
   console.log("Node app is running at localhost:" + app.get("port"));
 });
